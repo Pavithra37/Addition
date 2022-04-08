5ca31e2e92bb5741919caf1a0ca153aabb996765
dharshini <dharshini.e2019@vitstudent.ac.in>
2022-04-08 12:06:29 +0530

    Adding second commit to repo

--- Addition.java
+++ Addition.java
@@ -3,16 +3,16 @@ public class Addition {
 
     public static void main(String[] args) {
         
-        int num1, num2, sum;
+        int number1, number2, sum;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number: ");
-        num1 = sc.nextInt();
+        number1 = sc.nextInt();
         
         System.out.println("Enter Second Number: ");
-        num2 = sc.nextInt();
+        number2 = sc.nextInt();
         
         sc.close();
-        sum = num1 + num2;
+        sum = number1 + number2;
         System.out.println("Sum of these numbers: "+sum);
     }
 }
